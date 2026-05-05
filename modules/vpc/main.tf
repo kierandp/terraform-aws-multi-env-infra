a5e9bb767cb613e8c811e6548e9c75dd31107c6b
kierandp <kierandp031102@gmail.com>
2026-05-05 14:54:37 +0800

    test

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = each.value.cidr
   availability_zone       = each.value.az
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = merge(var.tags, {
     Name = each.value.name
